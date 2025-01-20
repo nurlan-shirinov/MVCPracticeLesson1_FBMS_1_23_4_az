66213af66b617f7338f69ee9ebd062f84ae8c2d2
Shirinov Nurlan Anar <shirinov_n@itstep.edu.az>
2025-01-20 10:09:18 +0400

    db context implemented

---
+++ MVCPracticeLesson1/Context/SchoolDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCPracticeLesson1.Context
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ MVCPracticeLesson1/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCPracticeLesson1.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+    }
+}
